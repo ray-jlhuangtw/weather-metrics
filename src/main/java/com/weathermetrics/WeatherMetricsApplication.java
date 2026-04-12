2778cbbd5a23a200dd0010ffac06b962bf538eb6
rhuang <rhuang@rxsense.com>
2026-04-12 14:36:40 +0100

    init project

---
+++ src/main/java/com/weathermetrics/WeatherMetricsApplication.java
@@ -0,0 +1,12 @@
+package com.weathermetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMetricsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherMetricsApplication.class, args);
+    }
+}
---
+++ src/test/java/com/weathermetrics/WeatherMetricsApplicationTest.java
@@ -0,0 +1,12 @@
+package com.weathermetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMetricsApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
